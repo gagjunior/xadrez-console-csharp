7b873deb007451bb294ca6c8a4084e2adfc6385a
Gilberto <gilberto.junior@cbbasfaltos.com.br>
2022-01-11 17:46:20 -0300

    Começando implementar metodo que imprime o tabuleiro

--- xadrez-console-csharp/tabuleiro/Tabuleiro.cs
+++ xadrez-console-csharp/tabuleiro/Tabuleiro.cs
@@ -12,5 +12,10 @@
             Colunas = colunas;
             _peca = new Peca[Linhas, Colunas];
         }
+
+        public Peca Peca(int linha, int coluna) 
+        { 
+            return _peca[linha, coluna]; 
+        }
     }
 }
